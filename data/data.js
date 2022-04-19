5bdf6633652d9453a0bf2e5657e829929e37fc38
Aman21591 <19BCS2461@gmail.com>
2022-04-19 21:53:14 +0530

    Initial Commit

---
+++ data/data.js
@@ -0,0 +1,12 @@
+const data = [ 
+{ "id": 9090, "name": "Item1", "price": 200, "discount": 10, "type": "fiction","quantity":1, "img_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/HackerEarth_logo.png/220px-HackerEarth_logo.png" }, 
+{ "id": 9091, "name": "Item2", "price": 250, "discount": 15, "type": "literature","quantity":1, "img_url": "https://m.economictimes.com/thumb/msid-57387551,width-1200,height-900,resizemode-4,imgsize-4974333/hackerearth-now-seeks-coders-across-seven-seas.jpg" },
+ { "id": 9092, "name": "Item3", "price": 320, "discount": 5, "type": "literature","quantity":1, "img_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/HackerEarth_logo.png/220px-HackerEarth_logo.png" }, 
+ { "id": 9093, "name": "Item4", "price": 290, "discount": 0, "type": "thriller","quantity":1, "img_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/HackerEarth_logo.png/220px-HackerEarth_logo.png" }, 
+ { "id": 9094, "name": "Item5", "price": 500, "discount": 25, "type": "thriller","quantity":1, "img_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/HackerEarth_logo.png/220px-HackerEarth_logo.png" }, 
+ { "id": 9095, "name": "Item6", "price": 150, "discount": 5, "type": "literature","quantity":1, "img_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/HackerEarth_logo.png/220px-HackerEarth_logo.png" }, 
+ { "id": 9096, "name": "Item7", "price": 700, "discount": 22, "type": "literature","quantity":1, "img_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/HackerEarth_logo.png/220px-HackerEarth_logo.png" }, 
+{ "id": 9097, "name": "Item8", "price": 350, "discount": 18, "type": "fiction","quantity":1, "img_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/HackerEarth_logo.png/220px-HackerEarth_logo.png" } 
+];
+
+export default data;
